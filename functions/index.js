1825cd926e255acb76a77320ddffb21de9dcb6ec
RuTa31 <50747452+RuTa31@users.noreply.github.com>
2022-09-26 09:48:21 +0800

    CI CD

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// exports.onUserCreate = functions.firestore
+//   .document("users/{userId}")
+//   .onCreate(async (snapshot, context) => {
+//     const { userId } = context.params;
+//     console.log(userId, "=====", snapshot.data().email);
+//   });
--- src/page/login/SignUp.jsx
+++ src/page/login/SignUp.jsx
@@ -13,7 +13,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link } from "react-router-dom";
-import { useNavigation } from "react";
+import { useNavigation } from "react";  
 
 const theme = createTheme();
 
